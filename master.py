7249fc826393a5febda2d251be8ebfd5ea584efd
Ray Cai <kkppccdd@gmail.com>
2015-11-24 22:56:41 +0800

    #1 add uni test TaskFetcherTest

--- master.py
+++ master.py
@@ -1,15 +1,55 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 '''
 Created on 2015年11月15日
 
 @author: kkppccdd
 '''
 
+import Queue
+from worker import Worker,TaskFetcher,OutputPusher
+
+
+import boto3
+
 class Master(object):
     '''
         control all workers
     '''
-    def __init__(self):
+    def __init__(self,params):
+        self._params = params
+        self._requestQueue = Queue.Queue(256)
+        self._outputQueue = Queue.Queue(1024)
+        
+        self._workers = [];
+        self._task_fetcher = None
+        self._output_pusher = None
+        
+        self._prepare_worker(self._params['num_of_worker'])
+        self._prepare_task_fetcher()
+        self._prepare_output_pusher()
+    
+    def _prepare_worker(self,num_of_workers=1):
+        for num in range(0,num_of_workers):
+            worker = Worker(str(num),self._requestQueue,self._outputQueue)
+            self._workers.append(worker)
+    def _prepare_task_fetcher(self):
+        # get upstream queue
+        sqs = boto3.resource('sqs')
+        aws_task_queue = sqs.get_queue_by_name(QueueName=self._params["sqs_request_queue_name"])
+        self._task_fetcher = TaskFetcher(aws_task_queue,self._requestQueue)
+    def _prepare_output_pusher(self,):
+        # get downstream qeuue
+        sqs = boto3.resource('sqs')
+        aws_completion_event_queue = sqs.get_queue_by_name(QueueName=self._params["sqs_completion_queue_name"])
+        self._output_pusher = OutputPusher(self._outputQueue,aws_completion_event_queue)
+    def start(self):
+        self._task_fetcher.start()
+        self._output_pusher.start()
+        
+        for worker in self._workers:
+            worker.start()
+            
+    def stop(self):
         pass
     
-    def prepare_worker(self,num_of_workers=1):
-        pass
--- worker.py
+++ worker.py
@@ -4,7 +4,10 @@ import os
 import sys
 import timeit
 
+import json
+
 # library imports
+import threading
 import numpy as np
 from skimage import img_as_ubyte
 
@@ -13,17 +16,84 @@ import caffe
 from style import StyleTransfer
 
 class ArtTask(object):
-    def __init__(self,params):
+    def __init__(self,identifier,params):
+        self.identifier = identifier
         self.params = params
         self.style_image = None
         self.content_image = None
         self.output_image = None
+
+        
+class TaskFetcher(threading.Thread):
+    def __init__(self,upstream_task_queue,downstream_task_queue):
+        super(TaskFetcher, self).__init__()
+        self._upstream_task_queue = upstream_task_queue
+        self._downstream_task_queue = downstream_task_queue
+        self._stop_flag = False
+    def run(self):
+        """
+            continuously fetch event from upstream queue, wrapper it as ArtTask, the push downstream task queue
+        """
+        logging.info('start fetch task')
+        while(self._stop_flag != True):
+            for message in self._upstream_task_queue.receive_messages():
+                art_task_request = json.loads(message.body)
+                logging.debug('fetched task id: '+art_task_request['identifier'])
+                art_task = self._transform_to_art_task(art_task_request)
+                
+                self._downstream_task_queue.put(art_task)
+                
+                message.delete()
+        
+        logging.info('stop fetch task')
+    def _transform_to_art_task(self,art_task_request):
+        if('params' in art_task_request):
+            params = art_task_request['params']
+        else:
+            params = {
+                  "ratio":1e5,
+                  "num_iters":1,
+                  "length":512,
+                  "verbose":False
+                  }
+        identifier = art_task_request['identifier']
+        art_task = ArtTask(identifier,params)
+        
+        # load style image
+        
+        # load content image
+        
+        
+        return art_task
+    def stop(self):
+        """
+        stop this fetcher
+        """
+        self._stop_flag = True
+    
+class OutputPusher(threading.Thread):
+    def __init__(self,upstream_task_queue,downstream_task_queue):
+        self._upstream_task_queue = upstream_task_queue
+        self._downstream_task_queue = downstream_task_queue
+    def run(self):
+        """
+        listening on upstream task queue, upload output image to cloud storage and push completion event to downstream task queue if get taks event from upstream queue.
+        """
+        pass
+    def stop(self):
+        """
+        stop this worker
+        """
+        pass
     
 
-class Worker(object):
-    def __init__(self,upstream_task_queue,downstream_task_queue):
+class Worker(threading.Thread):
+    def __init__(self,name,upstream_task_queue,downstream_task_queue):
+        self._name = name
         self._upstream_task_queue  = upstream_task_queue
         self._downstream_task_queue = downstream_task_queue
+        
+        self._stop_flag = False
         
         caffe.set_mode_cpu()
         logging.info("Running net on CPU.")
@@ -38,19 +108,15 @@ class Worker(object):
         end = timeit.default_timer()
         logging.info("Ran {0} iterations in {1:.0f}s.".format(n_iters, end-start))
         img_out = self._style_transfer.get_generated()
-        #img_out = img_as_ubyte(img_out)
+        img_out = img_as_ubyte(img_out)
         return img_out;
-    
-    def start(self):
-        '''
-        start worker thread
-        '''
-        pass
-    
-    def _run(self):
+    def stop(self):
+        self._stop_flag = True
+    def run(self):
         '''
         '''
-        while True:
+        logging.info("Runing worker ("+self._name+")...")
+        while self._stop_flag == False:
             task = self._upstream_task_queue.get(False,)
             # validate task
             if self._validate_task(task) == False:
@@ -62,6 +128,6 @@ class Worker(object):
             
             self._downstream_task_queue.put(task)
             self._upstream_task_queue.task_done()
-            
+        logging.info("Completed worker ("+self._name+").")
     def _validate_task(self,task):
         return True  
